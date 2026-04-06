f639a803709e88165d4766bed9dd37c6ba181c37
NMOA <nunoalho77@gmail.com>
2026-04-06 19:03:09 +0100

    fix start file

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3"
-import { open } from "sqlite"
-
-export async function getDB() {
-  return open({
-    filename: "./prisma/dev.db",
-    driver: sqlite3.Database
-  })
-}
